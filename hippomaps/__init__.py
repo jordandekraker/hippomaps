60e6123c543758d7e26d2f766bb5fff2019df3dd
donnagift <94790814+donnagift@users.noreply.github.com>
2024-01-17 22:08:28 -0500

    Update __init__.py

--- hippomaps/__init__.py
+++ hippomaps/__init__.py
@@ -5,7 +5,6 @@ import hippomaps.utils
 import hippomaps.plotting
 import hippomaps.stats
 import hippomaps.fetcher
-from hippomaps.utils import profile_align
 
-__all__ = ['utils', 'plotting', 'stats', 'fetcher', 'profile_align']
+__all__ = ['utils', 'plotting', 'stats', 'fetcher']
 
