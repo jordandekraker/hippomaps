501bbc46808c195b7dbe1051a684f5b64b2cc6b3
donnagift <94790814+donnagift@users.noreply.github.com>
2024-01-17 21:56:57 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx_autodoc_typehints'
+    'sphinx.ext.intersphinx'
 ]
 
 intersphinx_mapping = {
