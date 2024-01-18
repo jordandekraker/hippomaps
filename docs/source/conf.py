e6b43b66b313ef326066bfe6c54eb5406445ff4e
donnagift <donnagiftcastro@gmail.com>
2024-01-18 14:15:16 -0500

    change path of tutorials

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,5 +44,5 @@ package_path = os.path.abspath('../..')
 os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 nbsphinx_prolog = """
-.. include:: ../../tutorials/tutorials.rst
+.. include:: ../../docs/source/tutorials.rst
 """
