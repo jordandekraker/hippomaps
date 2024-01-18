b656fa115cc25f3ca56e0556a632028fe2d1b7c9
Jordan DeKraker - B. Bernhardt Lab <jordand@bic.mni.mcgill.ca>
2024-01-18 13:36:37 -0500

    added config and updated ieeg plots and moved directory

--- hippomaps/__init__.py
+++ hippomaps/__init__.py
@@ -1,10 +1,11 @@
 """Neuroimaging statistics toolbox."""
 __version__ = "0.1.3"
 
+import hippomaps.config
 import hippomaps.utils
 import hippomaps.plotting
 import hippomaps.stats
 import hippomaps.fetcher
 
-__all__ = ['utils', 'plotting', 'stats', 'fetcher']
+__all__ = ['config', 'utils', 'plotting', 'stats', 'fetcher']
 
