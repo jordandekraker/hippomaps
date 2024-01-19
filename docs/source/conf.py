556d8d03495c54569ce543dd852b7a8a4bb6cea8
donnagift <donnagiftcastro@gmail.com>
2024-01-18 19:58:37 -0500

    tutorials rst notebooks paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,14 +12,14 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    'nbsphinx',
+    "sphinx_gallery.load_style"
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'nbsphinx',
-    "sphinx_gallery.load_style"
+    'sphinx.ext.intersphinx'
 ]
 
 intersphinx_mapping = {
@@ -37,9 +37,10 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-#path to the root of hippomaps  relative to the documentation root
+# path to the root of hippomaps  relative to the documentation root
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../../'))
 package_path = os.path.abspath('../..')
 os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
