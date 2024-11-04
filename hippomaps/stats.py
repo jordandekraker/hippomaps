4f867fde8071389f3c1f32e9db73968fba6a02e7
Enning Yang <enning2000@gmail.com>
2024-11-04 08:26:09 -0500

    bug in contextualize2D

--- hippomaps/stats.py
+++ hippomaps/stats.py
@@ -248,7 +248,7 @@ def contextualize2D(taskMaps, n_topComparison=3, nperm=1000, plotTable=True, plo
     if taskMaps.shape[0] != nV:
         taskMapsresamp = np.ones((nV,nT))*np.nan
         for t in range(nT):
-            taskMapsresamp[:,t],_,_ = hippomaps.utils.density_interp(hm.config.get_label_from_nV(taskMaps.shape[0]),'0p5mm',taskMaps[:,t], label='hipp')
+            taskMapsresamp[:,t],_,_ = hippomaps.utils.density_interp(hippomaps.config.get_label_from_nV(taskMaps.shape[0]),'0p5mm',taskMaps[:,t], label='hipp')
     else:
         taskMapsresamp = taskMaps
 
--- hippomaps/test_stats.py
+++ hippomaps/test_stats.py
@@ -2,9 +2,10 @@ import nibabel as nib
 import numpy as np
 import hippomaps as hm
 
+print("start test")
 map1 = nib.load("/Users/enningyang/Downloads/histology-Blockface_average-4_hemi-mix_den-unfoldiso_label-hipp.shape.gii").darrays[0].data
 map2 = nib.load("/Users/enningyang/Downloads/histology-Bieloschowsky_average-4_hemi-mix_den-unfoldiso_label-hipp.shape.gii").darrays[0].data
 
 map_set = np.stack((map1.reshape(-1,1),map2.reshape(-1,1)), axis=1)
 print(map_set.shape)
-# hm.stats.contextualize2D(map_set)
+hm.stats.contextualize2D(map_set)
